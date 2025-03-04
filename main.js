ca417ae56c1ec9fb6b439b9e54f515507d385e22
giorossi-elfo <144696023+giorossi-elfo@users.noreply.github.com>
2025-03-04 12:14:34 +0000

    Intro to Vue 3 - Creating the Vue App

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-const product = 'Socks'
+const vueApp = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks'
+        }
+    },
+    computed: {
+        productDescription() {
+            return `Description of ${this.product}`;
+        }
+    }
+})
